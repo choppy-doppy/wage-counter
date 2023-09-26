0acb2111abe98f6d46029c9bef82c01a1ebc0b2a
Choppy <kaedongeddes@sd28.org>
2023-09-26 09:00:27 -0700

    initial regular stopwatch yippie
    ahgajsg;l

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-int main() {
-    cout << "hello world" << endl;
+void displayTime(int hours, int minutes, int seconds, int milliseconds) {
+    cout << hours << ":"
+         << minutes << ":"
+         << seconds << ":"
+         << milliseconds << endl;
 }
+
+int main() {
+    int hour = 0;
+    int minute = 0;
+    int second = 0;
+    int millisecond = 00;
+
+    for ( ; ; ) {
+        Sleep(0.001);
+
+        millisecond++;
+
+        if(millisecond > 999) {
+            second++;
+            millisecond = 0;
+        }
+
+        if(second > 59) {
+            minute++;
+            millisecond = 0;
+            second = 0;
+            minute = 0;
+        }
+
+        if (minute > 59) {
+            hour++;
+            millisecond = 0;
+            second = 0;
+            minute = 0;
+        }
+
+        displayTime(hour, minute, second, millisecond);
+    }
+}
+
